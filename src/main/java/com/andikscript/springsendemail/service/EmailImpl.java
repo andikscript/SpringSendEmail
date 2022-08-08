0c63e20309194572ece303cd08bdf76381e462e9
andikscript <andikscript@gmail.com>
2022-08-08 12:13:09 +0700

    add service email with attachment

--- src/main/java/com/andikscript/springsendemail/service/EmailImpl.java
+++ src/main/java/com/andikscript/springsendemail/service/EmailImpl.java
@@ -1,10 +1,20 @@
 package com.andikscript.springsendemail.service;
 
 import com.andikscript.springsendemail.model.Email;
+import com.andikscript.springsendemail.model.EmailAttach;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.SimpleTimeZone;
 
 @Service
 public class EmailImpl implements EmailService {
@@ -28,6 +38,28 @@ public class EmailImpl implements EmailService {
             mailMessage.setSubject(email.getSubject());
 
             javaMailSender.send(mailMessage);
+            return "Mail sent success";
+        } catch (Exception e) {
+            throw new Exception();
+        }
+    }
+
+    @Override
+    public String sendEmailWithAttach(EmailAttach email) throws Exception {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            FileSystemResource fileSystemResource = new FileSystemResource(new File(email.getAttachment()));
+
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(email.getReceived());
+            mimeMessageHelper.setText(email.getMessage());
+            mimeMessageHelper.setSubject(email.getSubject());
+            mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+
+            javaMailSender.send(mimeMessage);
             return "Mail sent success";
         } catch (Exception e) {
             throw new Exception();
--- src/main/java/com/andikscript/springsendemail/service/EmailService.java
+++ src/main/java/com/andikscript/springsendemail/service/EmailService.java
@@ -1,8 +1,11 @@
 package com.andikscript.springsendemail.service;
 
 import com.andikscript.springsendemail.model.Email;
+import com.andikscript.springsendemail.model.EmailAttach;
 
 public interface EmailService {
 
     String sendEmail(Email email) throws Exception;
+
+    String sendEmailWithAttach(EmailAttach email) throws Exception;
 }
